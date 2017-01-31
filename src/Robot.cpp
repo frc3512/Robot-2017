45d3b00fd094f8f6db5540a0bb7929e1c337c374
Charlie Parkinson <charlesparkinson14@gmail.com>
2017-01-30 17:50:19 -0800

    Formatted robot template.
    Change-Id: Ibe875eceaf1892f219fee0589409e6a5691f39a4

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,3 +1,5 @@
+// Copyright (c) FRC Team 3512, Spartatroniks 2017. All Rights Reserved.
+
 #include "Robot.hpp"
 
 /**
--- src/Robot.hpp
+++ src/Robot.hpp
@@ -1,3 +1,5 @@
+// Copyright (c) FRC Team 3512, Spartatroniks 2017. All Rights Reserved.
+
 #pragma once
 
 #include <iostream>
@@ -5,15 +7,13 @@
 #include <string>
 
 #include <Joystick.h>
+#include <RobotDrive.h>
 #include <SampleRobot.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
-#include <RobotDrive.h>
 #include <Timer.h>
 
-class Robot : public SampleRobot{
+class Robot : public SampleRobot {
 public:
-
 private:
-
 };
