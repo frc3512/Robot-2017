2fc3a9a6b35c295624be16fc128b671a7c34aa9c
William Jin <wjin0702@yahoo.com>
2017-02-09 21:32:01 -0800

    Added untested gear pickup state machine
    Change-Id: I4f9d9501e9cc73e6864a804537e274ce38241fb2

---
+++ src/Subsystems/GearPickup.cpp
@@ -0,0 +1,73 @@
+// Copyright (c) FRC Team 3512, Spartatroniks 2016-2017. All Rights Reserved.
+
+#include "GearPickup.hpp"
+GearPickup::GearPickup(){
+
+		m_joystickEvent.RegisterButtonEvent("InitialTriggerPress", k_grabberStickPort,
+	                                      1, true);
+	    // Idle
+	    auto state = std::make_unique<State>("Idle");
+	    state->Entry = [this] {pickupSolenoid.Set(true); };
+	    state->CheckTransition = [this](const std::string& event) {
+	        if (event == "InitialTriggerPress") {
+	            return "Lower-Pickup";
+	        } else {
+	            return "";
+	        }
+	    };
+	    gearPickup.AddState(std::move(state));
+
+	    // Lower-Pickup
+	    state = std::make_unique<State>("Lower-Pickup");
+	    state->Run = [this] { gearMotor.Set(-0.3); };
+	    state->CheckTransition = [this](const std::string& event) {
+	        if (!DigitalInputHandler::Get(1)->Get()) {
+	            return "Close-Pickup";
+	        } else {
+	            return "";
+	        }
+	    };
+	    gearPickup.AddState(std::move(state));
+
+	    // Close-Pickup
+	    state = std::make_unique<State>("Close-Pickup");
+	    state->Entry = [this] {
+	    	pickupSolenoid.Set(false);
+	    	m_timerEvent.Reset();
+	    };
+	    state->CheckTransition = [this](const std::string& event) {
+	    	if (event == "SolenoidTimeOut"){
+		    	return "Raise-Pickup";
+	    	} else {
+	    		return "";
+	    	}
+	    };
+	    gearPickup.AddState(std::move(state));
+
+	    // Raise-Pickup
+	    state = std::make_unique<State>("Raise-Pickup");
+	    state->Run = [this] { gearMotor.Set(0.3); };
+	    state->CheckTransition = [this](const std::string& event) {
+	        if (!DigitalInputHandler::Get(0)->Get()) {
+	            return "Open-Pickup";
+	        } else {
+	            return "";
+	        }
+	    };
+	    gearPickup.AddState(std::move(state));
+
+	    // Open-Pickup
+	    state = std::make_unique<State>("Open-Pickup");
+	    state->CheckTransition = [this](const std::string& event) {
+	        if (event == "InitialTriggerPress") {
+	        	pickupSolenoid.Set(true);
+	            return "Idle";
+	        } else {
+	            return "";
+	        }
+	    };
+	    gearPickup.AddState(std::move(state));
+}
+void GearPickup::Pickup() {
+    gearPickup.Run();
+}
