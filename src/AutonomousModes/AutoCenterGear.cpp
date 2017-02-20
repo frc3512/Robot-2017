a603187442837ddb1811ace317cb639b342bd9cc
Tyler Veness <calcmogul@gmail.com>
2017-02-19 18:12:37 -0800

    Replaced drive train velocity PID with position PID
    Change-Id: Ib45e40f7e07a95113a98821db2fc08b1d67034e3

--- src/AutonomousModes/AutoCenterGear.cpp
+++ src/AutonomousModes/AutoCenterGear.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) FRC Team 3512, Spartatroniks 2016-2017. All Rights Reserved.
 
+#include <cmath>
+
 #include "../Robot.hpp"
 
 using namespace std::chrono_literals;
@@ -7,11 +9,22 @@ using namespace std::chrono_literals;
 /* Moves forwards a set distance and then stops with gear penetrated by
  * airship's divot*/
 void Robot::AutoCenterGear() {
+    robotDrive.StartClosedLoop();
+
+    robotDrive.ResetEncoders();
+    robotDrive.ResetGyro();
     shifter.Set(true);  // low gear
-    while (IsAutonomous() && IsEnabled() && 1 /*not at setpoint*/) {
+    robotDrive.SetPositionReference(114.3 - 39 /*robot length*/);
+    robotDrive.SetAngleReference(0);
+
+    while (IsAutonomous() && IsEnabled() &&
+           std::abs(robotDrive.GetPosReference() - robotDrive.GetPosition()) >
+               0.001) {
         DS_PrintOut();
-        robotDrive.Drive(0.75, 0.0, false);
+
         std::this_thread::sleep_for(10ms);
     }
+    robotDrive.StopClosedLoop();
+
     robotDrive.Drive(0.0, 0.0, false);
 }
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -12,12 +12,16 @@ Robot::Robot() {
 
     server.SetSource(camera1);
 
-    pidGraph.SetSendInterval(5ms);
+    pidGraph.SetSendInterval(50ms);
 
     robotGrabber.SetLimitOnHigh(false);
 }
 
 void Robot::OperatorControl() {
+    robotDrive.StopClosedLoop();
+
+    robotDrive.ResetEncoders();
+    robotDrive.ResetGyro();
     while (IsEnabled() && IsOperatorControl()) {
         // Drive Stick Controls
         if (driveStick1.GetTrigger()) {
@@ -26,6 +30,12 @@ void Robot::OperatorControl() {
         } else {
             robotDrive.Drive(driveStick1.GetY(), driveStick2.GetX(),
                              driveStick2.GetRawButton(2));
+        }
+
+        if (grabberStick.GetRawButton(4)) {
+            robotGrabber.Set(1);
+        } else if (grabberStick.GetRawButton(6)) {
+            robotGrabber.Set(-1);
         }
 
         if (drive2Buttons.PressedButton(1)) {
@@ -74,7 +84,9 @@ void Robot::Autonomous() {
     autoTimer.Reset();
     autoTimer.Start();
 
-    dsDisplay.ExecAutonomous();
+    AutoCenterGear();
+
+    // dsDisplay.ExecAutonomous();
 
     DS_PrintOut();
 }
@@ -104,9 +116,10 @@ void Robot::DS_PrintOut() {
 
         // pidGraph.GraphData(robotDrive.GetFilteredRate(), "Filtered Gyro");
 
-        pidGraph.GraphData(robotDrive.GetVelocity(), "Velocity");
-        pidGraph.GraphData(k_driveMaxSpeed * -driveStick1.GetY(),
-                           "Velocity Ref");
+        pidGraph.GraphData(robotDrive.GetPosition(), "Position");
+        pidGraph.GraphData(93.3 * -driveStick1.GetY(), "Position Ref");
+        pidGraph.GraphData(robotDrive.GetAngle(), "Angle");
+        pidGraph.GraphData(/*30 * driveStick2.GetX()*/ 0, "Angle Ref");
 
         pidGraph.ResetInterval();
     }
