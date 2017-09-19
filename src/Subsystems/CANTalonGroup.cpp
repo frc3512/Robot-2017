a4e4fe8cab845321e363778e68b5a30bba9cffef
Tyler Veness <calcmogul@gmail.com>
2017-09-18 22:03:03 -0700

    Updated repo for 2018 WPILib
    * CtrlSys files were updated from the WPILib PR
    * GearBox was refactored into CANTalonGroup
    * SubsystemBase was removed
    * DriveTrain now uses DifferentialDrive::CurvatureDrive instead of a custom
      CheesyDrive implementation
    * Removed Utility files since the code that used it has been removed
    * Replaced SampleRobot usage with TimedRobot, which required rewriting some
      autonomous modes to use state machines
    * Replaced ButtonTracker with GetRawButtonPressed() and GetRawButtonReleased()
    * Updated to 2018 CTRE libraries
    
    Change-Id: I8a73716f4e342eb36c90601ccefbc31b8b101e3f

---
+++ src/Subsystems/CANTalonGroup.cpp
@@ -0,0 +1,98 @@
+// Copyright (c) 2016-2018 FRC Team 3512. All Rights Reserved.
+
+#include "CANTalonGroup.hpp"
+
+#include <memory>
+
+void CANTalonGroup::Set(double value) {
+    if (m_forwardLimit != nullptr && m_limitOnHigh == m_forwardLimit->Get() &&
+        value > 0) {
+        /* If stopping motor in same limit switch state that limit switch is in
+         * now and motor is rotating into limit switch
+         */
+        m_canTalons[0].get().Set(ControlMode::PercentOutput, 0.0);
+    } else if (m_reverseLimit != nullptr &&
+               m_limitOnHigh == m_reverseLimit->Get() && value < 0) {
+        /* If stopping motor in same limit switch state that limit switch is in
+         * now and motor is rotating into limit switch
+         */
+    } else {
+        if (m_feedbackDevice != FeedbackDevice::None) {
+            m_canTalons[0].get().Set(ControlMode::Position, value);
+        } else {
+            m_canTalons[0].get().Set(ControlMode::PercentOutput, value);
+        }
+    }
+}
+
+double CANTalonGroup::Get() const { return m_canTalons[0].get().GetSelectedSensorPosition(m_canTalons[0].get().GetDeviceID()); }
+
+void CANTalonGroup::SetInverted(bool isInverted) {
+    m_canTalons[0].get().SetInverted(isInverted);
+}
+
+bool CANTalonGroup::GetInverted() const {
+    return m_canTalons[0].get().GetInverted();
+}
+
+void CANTalonGroup::Disable() { m_canTalons[0].get().Set(ControlMode::PercentOutput, 0.0); }
+
+void CANTalonGroup::StopMotor() { Disable(); }
+
+void CANTalonGroup::PIDWrite(double output) {
+    if (m_forwardLimit != nullptr && m_limitOnHigh == m_forwardLimit->Get() &&
+        output > 0) {
+        /* If stopping motor in same limit switch state that limit switch is in
+         * now and motor is rotating into limit switch
+         */
+        m_canTalons[0].get().Set(ControlMode::PercentOutput, 0.0);
+    } else if (m_reverseLimit != nullptr &&
+               m_limitOnHigh == m_reverseLimit->Get() && output < 0) {
+        /* If stopping motor in same limit switch state that limit switch is in
+         * now and motor is rotating into limit switch
+         */
+        m_canTalons[0].get().Set(ControlMode::PercentOutput, 0.0);
+    } else {
+        m_canTalons[0].get().Set(ControlMode::PercentOutput, output);
+    }
+}
+
+double CANTalonGroup::GetPosition() const {
+    return m_canTalons[0].get().GetSelectedSensorPosition(m_canTalons[0].get().GetDeviceID()) * m_distancePerPulse;
+}
+
+double CANTalonGroup::GetSpeed() const {
+    // RPM * degrees/rev / (seconds/min)
+    return m_canTalons[0].get().GetSelectedSensorVelocity(m_canTalons[0].get().GetDeviceID()) * m_distancePerPulse / 60.0;
+}
+
+void CANTalonGroup::SetDistancePerPulse(double distancePerPulse) {
+    m_distancePerPulse = distancePerPulse;
+}
+
+void CANTalonGroup::SetFeedbackDevice(FeedbackDevice device) {
+    m_feedbackDevice = device;
+    m_canTalons[0].get().ConfigSelectedFeedbackSensor(device, m_canTalons[0].get().GetDeviceID(), 50);
+}
+
+void CANTalonGroup::ResetEncoder() { m_canTalons[0].get().SetSelectedSensorPosition(0, m_canTalons[0].get().GetDeviceID(), 50); }
+
+void CANTalonGroup::SetSensorDirection(bool reverse) {
+    m_isEncoderReversed = reverse;
+    m_canTalons[0].get().SetSensorPhase(m_isEncoderReversed);
+}
+
+bool CANTalonGroup::IsEncoderReversed() const { return m_isEncoderReversed; }
+
+void CANTalonGroup::SetLimitOnHigh(bool limitOnHigh) {
+    m_limitOnHigh = limitOnHigh;
+}
+
+void CANTalonGroup::SetSoftPositionLimits(double min, double max) {
+    m_min = min;
+    m_max = max;
+}
+
+TalonSRX& CANTalonGroup::GetMaster() { return m_canTalons[0].get(); }
+
+double CANTalonGroup::GetOutput() { return GetPosition(); }
--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -1,19 +1,20 @@
-// Copyright (c) 2016-2017 FRC Team 3512. All Rights Reserved.
+// Copyright (c) 2016-2018 FRC Team 3512. All Rights Reserved.
 
 #include "DriveTrain.hpp"
 
 #include <cmath>
 #include <iostream>
+#include <limits>
 
 DriveTrain::DriveTrain() {
-    m_sensitivity = k_lowGearSensitive;
+    m_drive.SetDeadband(k_joystickDeadband);
 
     m_rightGrbx.SetInverted(true);
 
     m_leftGrbx.SetSensorDirection(true);
 
-    m_leftGrbx.GetMaster()->SetFeedbackDevice(CANTalon::QuadEncoder);
-    m_rightGrbx.GetMaster()->SetFeedbackDevice(CANTalon::QuadEncoder);
+    m_leftGrbx.SetFeedbackDevice(FeedbackDevice::QuadEncoder);
+    m_rightGrbx.SetFeedbackDevice(FeedbackDevice::QuadEncoder);
 
     m_leftGrbx.SetDistancePerPulse(k_driveDpP);
     m_rightGrbx.SetDistancePerPulse(k_driveDpP);
@@ -21,8 +22,16 @@ DriveTrain::DriveTrain() {
     m_leftGrbx.Set(0.0);
     m_rightGrbx.Set(0.0);
 
-    m_posPID.SetOutputRange(-0.25, 0.25);
-    m_anglePID.SetOutputRange(-0.5, 0.5);
+    m_controller.GetPositionPID().SetPID(k_posP, k_posI, k_posD);
+    m_controller.GetAnglePID().SetPID(k_angleP, k_angleI, k_angleD);
+
+    m_controller.GetPositionPID().SetOutputRange(-0.25, 0.25);
+    m_controller.GetAnglePID().SetOutputRange(-0.5, 0.5);
+
+    m_controller.SetPositionTolerance(1.5,
+                                      std::numeric_limits<double>::infinity());
+    m_controller.SetAngleTolerance(1.5,
+                                   std::numeric_limits<double>::infinity());
 }
 
 int32_t DriveTrain::GetLeftRaw() const { return m_leftGrbx.Get(); }
@@ -30,120 +39,8 @@ int32_t DriveTrain::GetLeftRaw() const { return m_leftGrbx.Get(); }
 int32_t DriveTrain::GetRightRaw() const { return m_rightGrbx.Get(); }
 
 void DriveTrain::Drive(double throttle, double turn, bool isQuickTurn) {
-    // Modified Cheesy Drive; base code courtesy of FRC Team 254
-
-    throttle *= -1;
-
-    // Limit values to [-1 .. 1]
-    throttle = Limit(throttle, 1.0);
-    turn = Limit(turn, 1.0);
-
-    /* Apply joystick deadband
-     * (Negate turn since joystick X-axis is reversed)
-     */
-    throttle = ApplyDeadband(throttle, m_deadband);
-    turn = ApplyDeadband(turn, m_deadband);
-
-    double turnNonLinearity = k_turnNonLinearity;
-
-    /* Apply a sine function that's scaled to make turning sensitivity feel
-     * better. turnNonLinearity should never be zero, but can be close.
-     */
-    turn = std::sin(M_PI / 2.0 * turnNonLinearity * turn) /
-           std::sin(M_PI / 2.0 * turnNonLinearity);
-
-    double angularPower = 0.0;
-    double linearPower = throttle;
-    double leftPwm = linearPower, rightPwm = linearPower;
-
-    double negInertia = turn - m_oldTurn;
-    m_oldTurn = turn;
-
-    // Negative inertia!
-    double negInertiaScalar;
-    if (turn * negInertia > 0) {
-        negInertiaScalar = k_inertiaDampen;
-    } else {
-        if (std::fabs(turn) > 0.65) {
-            negInertiaScalar = k_inertiaHighTurn;
-        } else {
-            negInertiaScalar = k_inertiaLowTurn;
-        }
-    }
-
-    m_negInertiaAccumulator +=
-        negInertia * negInertiaScalar;  // adds negInertiaPower
-
-    // Apply negative inertia
-    turn += m_negInertiaAccumulator;
-    if (m_negInertiaAccumulator > 1) {
-        m_negInertiaAccumulator -= 1;
-    } else if (m_negInertiaAccumulator < -1) {
-        m_negInertiaAccumulator += 1;
-    } else {
-        m_negInertiaAccumulator = 0;
-    }
-
-    // QuickTurn!
-    if (isQuickTurn) {
-        if (std::fabs(linearPower) < 0.2) {
-            double alpha = 0.1;
-            m_quickStopAccumulator = (1 - alpha) * m_quickStopAccumulator +
-                                     alpha * Limit(turn, 1.0) * 5;
-        }
-
-        angularPower = turn;
-    } else {
-        angularPower =
-            std::fabs(throttle) * turn * m_sensitivity - m_quickStopAccumulator;
-        if (m_quickStopAccumulator > 1) {
-            m_quickStopAccumulator -= 1;
-        } else if (m_quickStopAccumulator < -1) {
-            m_quickStopAccumulator += 1;
-        } else {
-            m_quickStopAccumulator = 0.0;
-        }
-    }
-
-    // Adjust straight path for turn
-    leftPwm += angularPower;
-    rightPwm -= angularPower;
-
-    // Limit PWM bounds to [-1..1]
-    if (leftPwm > 1.0) {
-        // If overpowered turning enabled
-        if (isQuickTurn) {
-            rightPwm -= (leftPwm - 1.0);
-        }
-
-        leftPwm = 1.0;
-    } else if (rightPwm > 1.0) {
-        // If overpowered turning enabled
-        if (isQuickTurn) {
-            leftPwm -= (rightPwm - 1.0);
-        }
-
-        rightPwm = 1.0;
-    } else if (leftPwm < -1.0) {
-        // If overpowered turning enabled
-        if (isQuickTurn) {
-            rightPwm += (-leftPwm - 1.0);
-        }
-
-        leftPwm = -1.0;
-    } else if (rightPwm < -1.0) {
-        // If overpowered turning enabled
-        if (isQuickTurn) {
-            leftPwm += (-rightPwm - 1.0);
-        }
-
-        rightPwm = -1.0;
-    }
-    m_leftGrbx.Set(leftPwm);
-    m_rightGrbx.Set(rightPwm);
+    m_drive.CurvatureDrive(throttle, turn, isQuickTurn);
 }
-
-void DriveTrain::SetDeadband(double band) { m_deadband = band; }
 
 void DriveTrain::ResetEncoders() {
     m_leftGrbx.ResetEncoder();
@@ -165,23 +62,15 @@ double DriveTrain::GetLeftRate() const { return m_leftGrbx.GetSpeed(); }
 
 double DriveTrain::GetRightRate() const { return m_rightGrbx.GetSpeed(); }
 
-double DriveTrain::GetPosition() const {
-    return (m_leftGrbx.GetPosition() + m_rightGrbx.GetPosition()) / 2;
-}
+double DriveTrain::GetPosition() { return m_controller.GetPosition(); }
 
-double DriveTrain::GetAngle() const { return m_gyro.GetAngle(); }
+double DriveTrain::GetAngle() { return m_controller.GetAngle(); }
 
 double DriveTrain::GetAngularRate() const { return m_gyro.GetRate(); }
 
-void DriveTrain::StartClosedLoop() {
-    m_leftOutput.Start();
-    m_rightOutput.Start();
-}
+void DriveTrain::StartClosedLoop() { m_controller.Enable(); }
 
-void DriveTrain::StopClosedLoop() {
-    m_leftOutput.Stop();
-    m_rightOutput.Stop();
-}
+void DriveTrain::StopClosedLoop() { m_controller.Disable(); }
 
 void DriveTrain::SetPositionReference(double position) {
     m_posRef.Set(position);
@@ -189,17 +78,13 @@ void DriveTrain::SetPositionReference(double position) {
 
 void DriveTrain::SetAngleReference(double angle) { m_angleRef.Set(angle); }
 
-double DriveTrain::GetPosReference() const { return m_posRef.Get(); }
+double DriveTrain::GetPosReference() const { return m_posRef.GetOutput(); }
 
-double DriveTrain::GetAngleReference() const { return m_angleRef.Get(); }
+double DriveTrain::GetAngleReference() const { return m_angleRef.GetOutput(); }
 
-bool DriveTrain::PosAtReference() const {
-    return std::abs(GetPosReference() - GetPosition()) < 1.5;
-}
+bool DriveTrain::PosAtReference() const { return m_controller.AtPosition(); }
 
-bool DriveTrain::AngleAtReference() const {
-    return std::abs(GetAngleReference() - GetAngle()) < 1.5;
-}
+bool DriveTrain::AngleAtReference() const { return m_controller.AtAngle(); }
 
 void DriveTrain::ResetGyro() { m_gyro.Reset(); }
 
